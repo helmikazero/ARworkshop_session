4ca0bf28c4149ceb9d1e62ebac785c51673d99fe
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2021-11-05 21:31:00 +0700

    push5

---
+++ AR_workshop_session1/Assets/Scripts/ScriptMenembak.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMenembak : MonoBehaviour
+{
+    public GameObject peluruPrefab;
+
+    public float kecepatanPeluru = 10f;
+
+    public Transform arCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject peluruBaru = Instantiate(peluruPrefab,arCameraPos.position,arCameraPos.rotation);
+
+            peluruBaru.GetComponent<Rigidbody>().velocity = arCameraPos.TransformDirection(Vector3.forward * kecepatanPeluru);
+        }
+    }
+}
